240d6e7c1e1345db7e07ce5d7b72d177842d0b2f
danishmuaazkhan <devloperdanishkhan@gamil.com>
2023-05-07 14:05:38 +0530

    tailwind.config.js added

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mulish:["Mulish","sans-serif"]
+      },
+      colors: {
+        deepBlue:"#02042a",
+        lightblue:"#2b84ea",
+        lightBlue300:"#4b94ed",
+        lightBlue500:"#0b72e7",
+        greenLight:"#61cea6",
+        grayText:"#818597",
+        grayBlue:"#344a6c",
+        deepBlueHead:"#162f56",
+        gray2:"#525a76",
+      },
+    },
+  },
+  plugins: [],
+}
+
